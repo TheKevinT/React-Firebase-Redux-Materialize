ca42182b2c663ed3de2b38ba8c4012b8ce80eb96
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-25 22:37:12 -0500

    CDN Materialize , crear componentes y rutas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AuthRouter from "./Routers/AuthRouter";
 
 const App = () => {
   return (
     <>
       <h1>Aplicacion</h1>
+      <AuthRouter />
     </>
   );
 };
---
+++ src/Routers/AuthRouter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthRouter = () => {
+  return (
+    <>
+      <h1>Auth router</h1>
+    </>
+  );
+};
+
+export default AuthRouter;
